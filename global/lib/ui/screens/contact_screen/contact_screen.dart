e9b48da7a113660ef2f1d289ce4b35d0a03fb493
abbas ali <abbaskhankhan577@gmail.com>
2025-03-06 15:23:47 +0500

    working on uri launcher

--- global/lib/ui/screens/contact_screen/contact_screen.dart
+++ global/lib/ui/screens/contact_screen/contact_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:global/core/constant/color/colors.dart';
 import 'package:global/core/constant/string/string.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactScreen extends StatefulWidget {
   const ContactScreen({super.key});
@@ -25,9 +25,23 @@ class _ContactScreenState extends State<ContactScreen> {
     if (await FlutterContacts.requestPermission()) {
       List<Contact> fetchedContacts = await FlutterContacts.getContacts(
           withProperties: true, withPhoto: true);
+
+      List<Contact> filteredContacts = fetchedContacts
+          .where((contact) => contact.phones.isNotEmpty)
+          .toList();
+
       setState(() {
-        contacts = fetchedContacts;
+        contacts = filteredContacts;
       });
+    }
+  }
+
+  void _makePhoneCall(String phoneNumber) async {
+    final Uri uri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      // debugPrint("Could not launch $uri");
     }
   }
 
@@ -57,28 +71,32 @@ class _ContactScreenState extends State<ContactScreen> {
                 ? const Center(
                     child: CircularProgressIndicator(color: mainorange),
                   )
-                : contacts!.isEmpty
-                    ? const Center(child: Text("No contacts found."))
-                    : Expanded(
-                        child: ListView.builder(
-                          itemCount: contacts!.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            Uint8List? image = contacts![index].photo;
-                            return ListTile(
-                              leading: (image == null)
-                                  ? const CircleAvatar(
-                                      backgroundColor: mainorange,
-                                      child: Icon(Icons.person),
-                                    )
-                                  : CircleAvatar(
-                                      backgroundImage: MemoryImage(image),
-                                    ),
-                              title: Text(contacts![index].name.first),
-                              onTap: () {},
-                            );
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: contacts!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Uint8List? image = contacts![index].photo;
+                        String number = contacts![index].phones.isNotEmpty
+                            ? contacts![index].phones.first.number
+                            : "---";
+                        return ListTile(
+                          leading: (image == null)
+                              ? const CircleAvatar(
+                                  backgroundColor: mainorange,
+                                  child: Icon(Icons.person),
+                                )
+                              : CircleAvatar(
+                                  backgroundImage: MemoryImage(image),
+                                ),
+                          title: Text(contacts![index].displayName),
+                          subtitle: Text(number),
+                          onTap: () async {
+                            _makePhoneCall("+1234567890");
                           },
-                        ),
-                      ),
+                        );
+                      },
+                    ),
+                  ),
           ],
         ),
       ),
