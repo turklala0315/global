0a24def0c32d6c28d2824e652cac5a8697b77583
abbas ali <abbaskhankhan577@gmail.com>
2025-03-10 15:23:02 +0500

    country code added into add new number in add to contact screen

--- global/lib/ui/screens/add_to_contact_screen/add_to_contact.dart
+++ global/lib/ui/screens/add_to_contact_screen/add_to_contact.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:global/core/constant/color/colors.dart';
 import 'package:global/core/constant/string/string.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class AddToContact extends StatefulWidget {
   const AddToContact({super.key});
@@ -20,101 +21,113 @@ class _AddToContactState extends State<AddToContact> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        child: Column(
-          children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(
-                  Icons.close,
-                  size: 30,
-                  color: darkgrey,
-                ),
-              ),
-              IconButton(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                IconButton(
                   onPressed: () {
-                    if (formkey.currentState!.validate()) {
-                      print('all good');
-                    }
+                    Navigator.pop(context);
                   },
                   icon: const Icon(
-                    Icons.how_to_reg_outlined,
+                    Icons.close,
                     size: 30,
                     color: darkgrey,
-                  )),
-            ]),
-            //
-            SizedBox(height: 10.h),
-            //
-            Row(
-              children: [
-                Image.asset(
-                  '$staticAssets/logo.png',
-                  scale: 2,
-                )
-              ],
-            ),
-            Text(
-              'New Contact',
-              style: TextStyle(fontSize: 45.sp, fontWeight: FontWeight.bold),
-            ),
-            //
-            SizedBox(height: 20.h),
-            //
-            Form(
-                key: formkey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: name,
-                      decoration: const InputDecoration(
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        print('all good');
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.how_to_reg_outlined,
+                      size: 30,
+                      color: darkgrey,
+                    )),
+              ]),
+              //
+              SizedBox(height: 10.h),
+              //
+              Row(
+                children: [
+                  Image.asset(
+                    '$staticAssets/logo.png',
+                    scale: 2,
+                  )
+                ],
+              ),
+              Text(
+                'New Contact',
+                style: TextStyle(fontSize: 45.sp, fontWeight: FontWeight.bold),
+              ),
+              //
+              SizedBox(height: 20.h),
+              //
+              Form(
+                  key: formkey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: name,
+                        decoration: const InputDecoration(
+                            fillColor: lightgrey,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            labelText: 'First Name',
+                            labelStyle: TextStyle(color: mainorange),
+                            prefixIcon: Icon(Icons.person_2_outlined)),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return " Enter a valid name";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      //
+                      SizedBox(height: 30.h),
+                      //
+                      TextFormField(
+                        controller: name,
+                        decoration: const InputDecoration(
                           fillColor: lightgrey,
                           filled: true,
                           border: OutlineInputBorder(
                               borderSide: BorderSide.none,
                               borderRadius:
                                   BorderRadius.all(Radius.circular(20))),
-                          labelText: 'First Name',
+                          labelText: 'Last Name',
                           labelStyle: TextStyle(color: mainorange),
-                          prefixIcon: Icon(Icons.person_2_outlined)),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return " Enter a valid name";
-                        } else {
-                          return null;
-                        }
-                      },
-                    ),
-                    //
-                    SizedBox(height: 40.h),
-                    //
-                    TextFormField(
-                      controller: number,
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
+                        ),
+                      ),
+                      //
+                      SizedBox(height: 40.h),
+                      //
+                      IntlPhoneField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
                           fillColor: lightgrey,
                           filled: true,
+                          labelText: 'Phone Number',
                           border: OutlineInputBorder(
                               borderSide: BorderSide.none,
                               borderRadius:
                                   BorderRadius.all(Radius.circular(20))),
-                          prefix: Text('+92'),
-                          labelText: 'Phone',
-                          labelStyle: TextStyle(color: mainorange),
-                          prefixIcon: Icon(Icons.phone)),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return " Enter a valid name";
-                        } else {
-                          return null;
-                        }
-                      },
-                    )
-                  ],
-                ))
-          ],
+                        ),
+                        initialCountryCode: 'PK',
+                        onChanged: (phone) {
+                          print(phone.completeNumber);
+                        },
+                      )
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
